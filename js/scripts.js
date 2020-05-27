985b3297feb93fd2669237350956e21163cc9358
meeks21 <anthony.meeks3@gmail.com>
2020-05-26 20:58:49 -0700

    third commit

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+function callProductOne(){
+    var smoothie = document.getElementById('smoothies_pics').innerHTML;   //finds the image element that is going to be updated
+    var newContent = '<img src="images/frozen-fruit-smoothie_WH_360_392.jpg" alt="frozen_berry">';
+
+    document.getElementById('smoothies_pics').innerHTML = newContent;
+    
+}
+
+function callProductTwo(){
+    var smoothie = document.getElementById('smoothies_pics').innerHTML;   //finds the image element that is going to be updated
+    var newContent = '<img src="images/Strawberry-Mango-Smoothie_WH_360_392.jpg" alt="strawberry_mango">';
+
+    document.getElementById('smoothies_pics').innerHTML = newContent;
+    
+}
+
+function callProductThree(){
+    var smoothie = document.getElementById('smoothies_pics').innerHTML;   //finds the image element that is going to be updated
+    var newContent = '<img src="images/raspberry-peach-green-tea-smoothie_WH_360_392.jpg" alt="raspberry-peach-green-tea">';
+
+    document.getElementById('smoothies_pics').innerHTML = newContent;
+    
+}
+
+function callProductFour(){
+    var smoothie = document.getElementById('smoothies_pics').innerHTML;   //finds the image element that is going to be updated
+    var newContent = '<img src="images/Greek-yogurt-smoothie-peanut-butter_WH_360_392.jpg" alt="Greek-yogurt-peanut-butter-smoothie-_">';
+
+    document.getElementById('smoothies_pics').innerHTML = newContent;
+    
+}
+
+
